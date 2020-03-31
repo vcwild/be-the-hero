79b7db9f98951c3e957f9044ac573253f25ac212
vcwild <vcwildm@gmail.com>
2020-03-31 03:18:23 -0300

    updated database test sets

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -26,7 +26,7 @@ routes.post(
         [Segments.BODY]: Joi.object().keys({
             name: Joi.string().required(),
             email: Joi.string().required().email(),
-            whatsapp: Joi.string().required().length(11),
+            whatsapp: Joi.string().required().min(10).max(11),
             city: Joi.string().required(),
             uf: Joi.string().required().length(2),
         })
